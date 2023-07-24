e9b5f9dae80286a3c25122bb1ea0452755d67f35
Dissoloquele-Bengui <dissoloquelebengui67@gmail.com>
2023-07-24 23:35:50 +0100

    24/07/2023:23:30

--- resources/views/_form/_barCodeForm/index.blade.php
+++ resources/views/_form/_barCodeForm/index.blade.php
@@ -2,18 +2,18 @@
     <div class="col-md-5">
         <div class="form-group mb-3">
             <label for="qtd">Quantidade de Códigos</label>
-            <input required type="number" id="qtd" name="qtd" class="form-control" value="">
+            <input required type="number" id="qtd" name="qtd" class="form-control" value="" min="1">
         </div>
         <div class="form-group mb-3">
             <label for="altura">Altura dos Códigos</label>
-            <input required type="number" id="altura" name="altura" class="form-control" value="" step="any">
+            <input required type="number" id="altura" name="altura" class="form-control" value="" step="any" min="1">
         </div>
     </div> <!-- /.col -->
     <div class="col-md-5">
 
         <div class="form-group mb-3">
             <label for="largura">Largura</label>
-            <input required type="number" id="largura" name="largura" class="form-control" value="" step="any">
+            <input required type="number" id="largura" name="largura" class="form-control" value="" step="any" min="1">
         </div>
     </div>
 </div>
